4acde9d8adf4246a411d12a3ee560b12b6211112
Yana Sitkovets <asitkovec@gmail.com>
2023-05-25 17:07:46 +0300

    Add code

---
+++ maxwell_distribution/particle.py
@@ -0,0 +1,80 @@
+import numpy as np
+from matplotlib.patches import Circle
+
+class Particle:
+    """A class representing a two-dimensional particle."""
+
+    def __init__(self, x, y, vx, vy, radius=0.01, styles=None):
+        """Initialize the particle's position, velocity, and radius.
+
+        Any key-value pairs passed in the styles dictionary will be passed
+        as arguments to Matplotlib's Circle patch constructor.
+
+        """
+
+        self.r = np.array((x, y))
+        self.v = np.array((vx, vy))
+        self.radius = radius
+
+        self.styles = styles
+        if not self.styles:
+            # Default circle styles
+            self.styles = {'edgecolor': 'b', 'fill': False}
+
+    # For convenience, map the components of the particle's position and
+    # velocity vector onto the attributes x, y, vx and vy.
+    @property
+    def x(self):
+        return self.r[0]
+    @x.setter
+    def x(self, value):
+        self.r[0] = value
+    @property
+    def y(self):
+        return self.r[1]
+    @y.setter
+    def y(self, value):
+        self.r[1] = value
+    @property
+    def vx(self):
+        return self.v[0]
+    @vx.setter
+    def vx(self, value):
+        self.v[0] = value
+    @property
+    def vy(self):
+        return self.v[1]
+    @vy.setter
+    def vy(self, value):
+        self.v[1] = value
+
+    def overlaps(self, other):
+        """Does the circle of this Particle overlap that of other?"""
+
+        return np.hypot(*(self.r - other.r)) < self.radius + other.radius
+
+    def draw(self, ax):
+        """Add this Particle's Circle patch to the Matplotlib Axes ax."""
+
+        circle = Circle(xy=self.r, radius=self.radius, **self.styles)
+        ax.add_patch(circle)
+        return circle
+
+    def advance(self, dt):
+        """Advance the Particle's position forward in time by dt."""
+
+        self.r += self.v * dt
+
+        # Make the Particles bounce off the walls
+        if self.x - self.radius < 0:
+            self.x = self.radius
+            self.vx = -self.vx
+        if self.x + self.radius > 1:
+            self.x = 1-self.radius
+            self.vx = -self.vx
+        if self.y - self.radius < 0:
+            self.y = self.radius
+            self.vy = -self.vy
+        if self.y + self.radius > 1:
+            self.y = 1-self.radius
+            self.vy = -self.vy
